821f83b8609b703e87408cdc40696ff9c21c8001
Tyler Ellingford <ellingfordtyler@gmail.com>
2020-02-11 21:20:41 -0700

    Added register and login user actions to userReducer

--- redux/userReducer.js
+++ redux/userReducer.js
@@ -6,6 +6,8 @@ const initialState = {
 
 // ACTION TYPES
 const REQUEST_USER_DATA = 'REQUEST_USER_DATA';
+const REGISTER_USER = 'REGISTER_USER';
+const LOGIN_USER = 'LOGIN_USER';
 
 // ACTION EXPORTS
 export const requestUserData = () => {
@@ -16,10 +18,30 @@ export const requestUserData = () => {
   }
 }
 
+export const registerUser = (user) => {
+  // TODO: Create call to register users data and return created profile as payload.
+  return {
+    type: REGISTER_USER,
+    payload: {},
+  }
+}
+
+export const loginUser = (user) => {
+  // TODO: Create call to authenticate user and return user profile.
+  return {
+    type: LOGIN_USER,
+    payload: {},
+  }
+}
+
 // USER REDUCER
 export const userReducer = (state = initialState, action) => {
   switch(action.type) {
     case REQUEST_USER_DATA:
+      return state;
+    case REGISTER_USER:
+      return state;
+    case LOGIN_USER:
       return state;
     default:
       return state;
